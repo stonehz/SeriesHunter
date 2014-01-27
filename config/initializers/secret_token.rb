1d557479814606d96f37f2a22a353e9fc34af0da
Antonios N <stonehz@gmail.com>
2014-01-27 15:38:40 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeriesHunter::Application.config.secret_token = '52442011b2e897b24ddeb5f8a7eb4860439fa917e6d8eda84d1f4b90d67f78bef627b7712ebf8e5da2d9487d83c13856790b6d4a970814c59386fec6f63ae90a'
